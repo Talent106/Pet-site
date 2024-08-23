d813c389e084252570333e13c985e0acf4116edf
Kimura Yuu <joaovictor9910@outlook.com>
2024-08-23 05:07:06 -0300

    style: add the Gallery page and Carousl page

--- src/components/Landing/index.vue
+++ src/components/Landing/index.vue
@@ -14,13 +14,53 @@
                 </div>
             </div>
         </div>
+
+        <div class="banners">
+            <div v-for="(item, index) in items" :key="index" class="banner-x">
+                <img class="gallery" :src="item.img" />
+                <div class="round">
+                    <i :class="item.icon"></i>
+                </div>
+                <p>{{ item.title }}</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import '@/assets/scss/components/Landing.scss';
+    import img1 from '@/assets/img/1.jpg';
+    import img2 from '@/assets/img/2.jpg';
+    import img3 from '@/assets/img/3.jpg';
+    import img4 from '@/assets/img/4.jpg';
 
     export default {
         name: 'Landing',
+        data() {
+            return {
+                items: [
+                    {
+                        img: img1,
+                        icon: "bi bi-house-fill",
+                        title: "Treats"
+                    },
+                    {
+                        img: img2,
+                        icon: "bi bi-palette-fill",
+                        title: "Bones & Chews"
+                    },
+                    {
+                        img: img3,
+                        icon: "bi bi-puzzle-fill",
+                        title: "Bowls & Diners"
+                    },
+                    {
+                        img: img4,
+                        icon: "bi bi-inboxes-fill",
+                        title: "Accessories"
+                    },
+                ]
+            }
+        }
     }
 </script>
