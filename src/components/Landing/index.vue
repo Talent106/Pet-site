e855f7471a090cf5bd924caa99b6cfebf50f5fdc
Kimura Yuu <joaovictor9910@outlook.com>
2024-08-23 07:32:19 -0300

    style: landing page complete

--- src/components/Landing/index.vue
+++ src/components/Landing/index.vue
@@ -26,6 +26,19 @@
         </div>
 
         <Products></Products>
+
+        <div class="newsletter">
+            <h3 class="bold capslock me-4">Newsletter Signup</h3>
+            <div class="input-group mx-4">
+                <input type="text" class="form-control" placeholder="Email Address*">
+                <div class="input-group-append">
+                    <button class="btn btn-danger capslock bold" type="button">
+                        Subscribe
+                    </button>
+                </div>
+            </div>
+            <img src="../../assets/img/dog.webp" />
+        </div>
     </div>
 </template>
 
