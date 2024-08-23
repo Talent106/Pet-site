5ccab943e26511d580ebb747fab327b7979a5ec9
Kimura Yuu <joaovictor9910@outlook.com>
2024-08-23 08:19:00 -0300

    style: add footer top info bar

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,28 +1,48 @@
 <template>
     <div class="footer">
         <div class="info">
-            <img class="logo me-4" src="../assets/img/lp_logo_1_1706041538__38057.webp" />
-            
+            <img class="logo me-4 mb-3" src="../assets/img/lp_logo_1_1706041538__38057.webp" />
+
             <div class="footer-info mx-4">
-                <div class="d-flex text-white mb-3">
+                <div class="d-flex text-white mb-3 f-13">
                     <i class="bi bi-geo-alt-fill text-danger me-2"></i>
                     110 Melrich Rd, Suite 4 Cranbury , NJ 08512 United States Of America
                 </div>
-                <div class="d-flex text-white mb-3">
+                <div class="d-flex text-white mb-3 f-13">
                     <i class="bi bi-geo-alt-fill text-danger me-2"></i>
                     (609) 655-3700
                 </div>
-                <div class="d-flex text-white">
+                <div class="d-flex text-white f-13 bold">
                     <img class="flag me-2" src="../assets/img/australian-flag-icon.jpg" />
                     For Our Australian Customers
                 </div>
             </div>
 
-            <div class="categories bold text-white">
-                <h6 class="capslock">categories</h6>
+            <div class="information bold mx-4">
+                <h6 class="capslock text-white">categories</h6>
                 <a href="">New!</a>
                 <a href="">Dog</a>
                 <a href="">Cat</a>
+            </div>
+
+            <div class="information bold mx-4">
+                <h6 class="capslock text-white">information</h6>
+                <a href="">About</a>
+                <a href="">Blog</a>
+                <a href="">Contact Us</a>
+                <a href="">Stor Locator</a>
+                <a href="">Team & Conditions</a>
+                <a href="">Contact Us</a>
+            </div>
+
+            <div class="information bold mx-4">
+                <h6 class="capslock text-white">Brand</h6>
+                <a href="">Dolce</a>
+                <a href="">Bella Bowls</a>
+                <a href="">Nature Choice</a>
+                <a href="">Pure Buffalo</a>
+                <a href="">Gorumet</a>
+                <a href="">Active Pets</a>
             </div>
         </div>
     </div>
